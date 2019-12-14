422644478cff045d65347b66eedb0e834d469921
AllySantos <alichia09@hotmail.com>
2019-12-14 00:50:18 +0000

    add conexao com mongodb

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//iniciando o app
 const app = express();
 
+
+//primeira rota
+app.get('/', function (req, res){ //toda vez q acessar o diretorio raiz (/)
+
+    res.send("sssa");
+})
+//conexao o bamco
+async function coneta(){
+
+     try {
+            await mongoose.connect('mongodb+srv://adm:adm@node-s2awl.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology: true});
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+coneta();
 
 //ouve a porta 3001
 
